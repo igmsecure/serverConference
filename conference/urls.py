bb2b2647496dbdd9b03b52f7163b9b34de9405a8
Ibragim Dzaurov <100013546+igmsecure@users.noreply.github.com>
2024-01-15 13:46:00 +0300

    Update urls.py

--- conference/urls.py
+++ conference/urls.py
@@ -4,12 +4,6 @@ from .views import *
  
 urlpatterns = [
     # path('', index),
-
     path('authors/', index, name='authors'),
     path('authors/<int:id>/', author, name='author'), 
-    path('filter/', filter, name='filter'),
-
-    path('addpage/', addpage, name='add_page'),
-    path('contact/', contact, name='contact'),
-    path('login/', login, name='login'),
 ]
